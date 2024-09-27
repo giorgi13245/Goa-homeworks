f7f12cfac147b0270db937ed3d0954982a9f8ec7
giorgi13245 <giorgibokoveli10@gmail.com>
2024-09-27 23:14:39 +0400

    first commit

---
+++ day 0/lesson 2.py
@@ -0,0 +1,28 @@
+name = "giorgi"
+
+
+surname = "boyoveli"
+
+#print(name)
+#print(surname)
+
+
+
+name = "giorgi" #str (string) ტიპის ცვლადი
+age = 14 #int (integer) ტიპის ცვლადი
+height = 168.5 #float ტიპის ცვლადი (ათწილადი)
+#booLean (booL) ტიპის ცვლადი
+
+knows_proggramming = True #True ან  False
+is_ugly = False    #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False   # ჯავასკრიპტული cameLcase
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_proggramming))
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(knows_proggramming))
---
+++ day 0/lesson 3.py
@@ -0,0 +1,50 @@
+Name=input("please enter your name")
+Surname=input("please enter your friends surname")
+print(Name + " " + Surname )
+
+#end of homework N1
+
+
+age=input("please enter your age")
+print(int(age) + 10)
+
+#end of homework N2
+
+
+numbers=[]
+for i in range(5):
+    num=float(input("please enter 5 numbers"))
+
+numbers.append(sum)
+
+average=sum(numbers)/len(numbers)
+
+print(average)
+
+
+#end of homework N3
+
+
+
+
+
+
+# case sensitive ნიშნავს იმას რომ თუ რამოდენიმე ცვლადს ერთი და იგივე სახელს დავარქმევთ მაგრამ განსხვავებული ზომის ასოებით მაგალითად name,Name და NAME პითონი მათ განსხვავებულ ცვლადებად აღითქვამს
+#snake case არის წერის სტილი როდესაც წინადადებაში ორ ან მეტ სიტყვას ქვედატირით ვანცალკევებთ
+
+#end of homework N4
+
+
+print(hello world) #print("hello world")  ბრძანებას აკლია ბრჭყალები ფრჩხილებში და ამის გამო პითონმა ის ცვლადად აღითქვა რომელიც არ შექმნილა Syntax error
+
+print(5+ "5")   #print(5+int("5"))     ჩვენ არ შეგვიძლია ინტეჯერს (int) დავუმატოთ სტრინგი (str) Type error
+
+name=input("please enter your name")
+print(Name)   #print(name) name დიდი ასოთი იქნა დაწერილი რომელიც პითონმა განსხვავებულ ცვლადად აღითქვა და გვითხრა რომ ასეთი ცვლადი არ შექმნილა
+
+city --> "paris" # city="paris" ბრძანებაში მიმნიჭებელი ოპერატორი უნდა იყოს ტოლობა და არა სხვა სიმბოლო
+
+name = gio #ცვლადის მნიშვნელობას აკლია ბრჭყალები
+
+
+#end of homework
