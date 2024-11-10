41a5252163545f54b02eda896d0d3523bf9fa855
giorgi13245 <giorgibokoveli10@gmail.com>
2024-11-10 14:53:59 +0400

    second commit

---
+++ day 10/classwork.py
@@ -0,0 +1,35 @@
+for i in range(51,150,2):
+    print(i)
+
+
+word=input('enter your sentence')
+for i in word:
+    print(i)
+
+
+
+
+i2=500
+while i2<700:
+    print(i2)
+    i2=i2+2
+
+
+
+
+i3=50
+while i3>0:
+    print(i3)
+    i3=i3-1
+
+
+
+
+fav_num=5
+user_input=''
+while fav_num != user_input:
+    user_input=int(input("enter your number"))
+
+
+
+
---
+++ day 10/homework.py
@@ -0,0 +1,35 @@
+number=input("enter your number")
+for i in number:
+    print(i)
+
+
+
+number2=int(input("enter your number"))
+for i in range(number2,0,-1):
+    print(i)
+
+
+
+
+i=1
+while i<100:
+    print(i)
+    i=i+2
+
+
+
+
+for i in range(1,100,2):
+    print(i)
+
+
+
+
+#for და while loops გამოიყენება იტერაციის შესაქმნელად, მათი საშუალებით კოდი რამოდენიმეჯერ მეორდება, მათ შორის განსხვავება ისაა რომ for loop ვიყენებთ მაშინ როდესაც ვიცით რამდენჯერ უნდა გამეორდეს კოდი, while loop ლი პირიქით.i არის საიტერაციო ცვლადი, რომელიც ითვლის, მერამდენე ბრუნზეა კოდი 
+    
+
+
+
+
+
+    
