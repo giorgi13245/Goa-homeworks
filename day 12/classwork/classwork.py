179b5b20a2e27993c424beafeb54c4b27ef7165c
giorgi13245 <giorgibokoveli10@gmail.com>
2024-11-28 17:53:00 +0400

    second commit

---
+++ day 12/classwork/classwork.py
@@ -0,0 +1,16 @@
+list=["xo","mo","do","fo","eo","bo","vo","po","ko","zo"]
+index=int(input("enter index"))
+print(list[index])
+
+
+
+
+list2=["gia","mia","lia","gio","nika"]
+index2=int(input("enter index"))
+name=input("enter your name")
+list2[index2]=name
+print(list2[index2])
+
+
+#mutable არის ცვლადი ანუ მისი მნიშვნელობა შეიძლება შეიცვალოს
+#immutable არის უცვლადი, ანუ მისი მნიშვნელობა შეუცვლელია
