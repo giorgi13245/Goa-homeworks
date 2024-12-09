079b43fcb7f5802efa91765960e9289191e24aca
giorgi13245 <giorgibokoveli10@gmail.com>
2024-12-09 17:43:38 +0400

    second commit

---
+++ day 14/classwork.py
@@ -0,0 +1,16 @@
+list=["a","b","c","d","e","f","g","h","i","j"]
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+print(list[num1:num2])
+
+
+
+
+list2=[1,2,3,4,5]
+num=int(input("enter number"))
+print(list2[num])
+
+
+name=input("enter name")
+my_name="giorgi"
+print(name[0:3]+my_name[0:3])
