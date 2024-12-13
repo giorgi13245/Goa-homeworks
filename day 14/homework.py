8874eca4aeb54283363e8f6fa2243a8766d6519b
giorgi13245 <giorgibokoveli10@gmail.com>
2024-12-13 22:08:00 +0400

    second commit

---
+++ day 14/homework.py
@@ -0,0 +1,31 @@
+days=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+print(days[0::6])
+days[2]="midweek"
+
+
+animal1=input("enter animal")
+animal2=input("enter animal")
+animal3=input("enter animal")
+animal4=input("enter animal")
+animal5=input("enter animal")
+animals=[animal1,animal2,animal3,animal4,animal5]
+print(animals[0::4])
+animals[1]=input("enter new animal")
+print(animals)
+
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10,11,12]
+print(numbers[2::3])
+print(numbers[::-1])
+
+
+
+numbers1=[1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500]
+print(numbers1[0:5])
+print(numbers1[2::3])
+
+
+numberss=(input("შეიყვანე 10 რიცხვი მძიმით გამოყოფით"))
+number_list=[numberss]
+print(numberss[0:3])
