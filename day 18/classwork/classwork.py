23f9c9eb7ef727d9669ce5dddcb3a2feb6389be4
giorgi13245 <giorgibokoveli10@gmail.com>
2025-01-13 19:19:18 +0400

    second commit

---
+++ day 18/classwork/classwork.py
@@ -0,0 +1,13 @@
+def add(num1,num2):
+    return num1 + num2 
+
+
+print(add(10,40))
+
+
+
+def substract(num3,num4):
+    return num3 - num4
+
+
+print(substract(30,12))
---
+++ day 18/homework/homework.py
@@ -0,0 +1,36 @@
+def mult(num1,num2):
+    return num1*num2
+
+
+print(mult(2,6))
+
+
+
+def check(num):
+    if num%2==0:
+          print("even")
+    else:
+         print("odd")
+
+
+
+check(4)
+
+
+
+def greet(name):
+    return print("hello" + " " + name)
+
+
+print(greet("nika"))
+print(greet("luka"))
+
+
+
+
+
+def connect(name1,name2):
+    return print(name1 + name2)
+
+
+connect('today','monday')
